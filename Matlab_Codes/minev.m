1c9e5614ad72a4dd9dec01d8545ae1b0f0b4e457
rueffl <87005641+rueffl@users.noreply.github.com>
2024-01-17 11:21:18 +0100

    Delete Matlab_Codes directory

--- Matlab_Codes/minev.m
+++
@@ -1,7 +0,0 @@
-% get the smalles eigenvalue of the matrix A
-function ev_min = minev(A)
-%     ev = eig(A);
-%     [ev_abs,order] = sort(abs(ev),'ascend');
-%     ev_min = ev(order(1));
-    ev_min = svds(A,1,"smallest");
-end
