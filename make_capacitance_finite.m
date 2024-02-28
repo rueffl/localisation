5c48ec3288ae4e1c622efe4f2475a903645ff6ac
Liora Rueff <liora.rueff@sam.math.ethz.ch>
2024-02-28 09:42:09 +0100

    added code for finite capacitance matrix

---
+++ make_capacitance_finite.m
@@ -0,0 +1,14 @@
+function C = make_capacitance_finite(N,lij)
+% MAKE_CAPACITANCE construct the capacitance matrix
+%   N:      number of resonators
+%   lij:    spacing between the resonators
+
+    if N == 2
+        D1 = cat(2,[1/lij(1)],[1/lij(1)]);
+    elseif N > 2
+        D1 = cat(2,[1/lij(1)],1./lij(1:end-1)+1./lij(2:end),[1/lij(end-1)]);
+    end
+    D2 = -1./lij;
+    C = diag(D1) + diag(D2,1) + diag(D2,-1);
+
+end
